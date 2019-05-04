16d8c8a62d3135c04ac177c4cedb9582eeb734bc
avu916 <andrewvu.cs@gmail.com>
2019-05-04 01:20:09 -0700

    updating states, with onClick

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Andrew', age: 29},
+      {name: 'Vu', age: 28},
+      {name: 'Michelle', age: 26}
+    ],
+    otherState:'some other value'
+  }
+
+  switchNameHandler = () =>{
+    //DON'T DO THIS: this.state.persons[0].name ='Maximillion';
+    this.setState({persons: [
+      {name: 'Andrews', age: 28},
+      {name: 'drew', age: 29},
+      {name: 'Stephanie', age: 27}
+      ] 
+    })
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Raciging </Person>
-        <Person name="Michelle" age="26"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Raciging </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
