80f8c369ad8f3584ca83a4d8d7ff7b4aa8865dfe
avu916 <andrewvu.cs@gmail.com>
2019-05-06 14:47:45 -0700

    using Radium for styling

--- src/App.js
+++ src/App.js
@@ -1,74 +1,109 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 
 class App extends Component {
   state = {
     persons: [
-      { name: 'Andrew', age: 28 },
-      { name: 'Vu', age: 29 },
-      { name: 'Michelle', age: 26 }
+      { id: 'asfa1', name: 'Andrew', age: 28 },
+      { id: 'vasdf1', name: 'Vu', age: 29 },
+      { id: 'aplesmf', name: 'Michelle', age: 26 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: 'false'
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Orange', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    })
-  };
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    });
 
-  nameChangedHandler = (event) => {
+    const person =  {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[PersonIndex])
+    person.name = event.target.value;
+
+    const persons=[...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: 'Andrew', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
+      persons: persons
     })
+  }
+
+  deletPersonHandler = (personIndex) =>{
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
     const style = {
-      backgroundColor: '#eeef3c',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover':{
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+      <div>
+        {this.state.persons.map((person, index) => {
+          return <Person 
+            click={() => this.deletPersonHandler(index)}
+            name={person.name} 
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)} />
+        })}
+      </div> 
+      );
+
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'red',
+        color: 'black'
+      };
+    }
+
+    const classes = [];
+    if (this.state.persons.length <=2){
+      classes.push('red');  // classses = ['red']
+    }
+    if (this.state.persons.length <=1){
+      classes.push('bold'); // classes = ['red', 'bold']
+    } 
+
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button 
           style={style}
-          onClick={() => this.switchNameHandler('apples!!!!')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Orangatang')}
-          changed={this.nameChangedHandler}>
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+          onClick={this.togglePersonsHandler}>Toggle Names</button>
+          {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default Radium(App);
