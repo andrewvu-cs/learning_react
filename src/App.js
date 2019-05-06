c61a99f4322e7905072f86d1f28db874baa3bf11
avu916 <andrewvu.cs@gmail.com>
2019-05-06 15:47:01 -0700

    Incorporated CSS Modules, Pracice using Pseuo selectors and Media Queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -47,17 +47,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    };
-
     let persons = null;
-
+    let btnClass = '';
     if (this.state.showPersons) {
       persons = (
       <div>
@@ -71,27 +62,24 @@ class App extends Component {
         })}
       </div> 
       );
-
-      style.backgroundColor = 'red';
-      
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <=2){
-      classes.push('red');  // classses = ['red']
+      assignedClasses.push(classes.red);  // classses = ['red']
     }
     if (this.state.persons.length <=1){
-      classes.push('bold'); // classes = ['red', 'bold']
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     } 
 
 
     return (
       
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button 
-            style={style}
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button className = {btnClass}
             onClick={this.togglePersonsHandler}>Toggle Names</button>
             {persons}
         </div>
